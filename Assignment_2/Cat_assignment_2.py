ef32ec593d2dcc4846f4d1a9d0031ae0c18604dd
caterinaburanelli <caterina.buranelli@studenti.unipd.it>
2021-03-04 16:14:16 +0100

    data

--- Assignment_2/Cat_assignment_2.py
+++ Assignment_2/Cat_assignment_2.py
@@ -56,12 +56,12 @@ def asian_MC(S,N,T,sigma,r,K, n, type_op, param):
     for i in range(n):
         S_ti_ar = []
         S_ti_geo = []
-        ST = ST_g = ST_a = S
+        ST = S
 
         if type_op == "arithmetic":
             for j in range(int(N)):
                 Z = np.random.normal()
-                ST = ST + (np.exp( (r-0.5*sigma**2)*T_i + sigma*np.sqrt(T_i)*Z))
+                ST = ST * (np.exp( (r-0.5*sigma**2)*T_i + sigma*np.sqrt(T_i)*Z))
                 S_ti_ar.append(ST)
         
             payoff.append(max(np.mean(S_ti_ar)-K, 0))
@@ -81,11 +81,9 @@ def asian_MC(S,N,T,sigma,r,K, n, type_op, param):
             for j in range(int(N)):
                 Z = np.random.normal()
                 
-                ST_g = ST_g * (np.exp( (r-0.5*sigma**2)*T_i + sigma*np.sqrt(T_i)*Z))
-                S_ti_geo.append(ST_g)
-                
-                ST_a = ST_a + (np.exp( (r-0.5*sigma**2)*T_i + sigma*np.sqrt(T_i)*Z))
-                S_ti_ar.append(ST_a)
+                ST = ST * (np.exp( (r-0.5*sigma**2)*T_i + sigma*np.sqrt(T_i)*Z))
+                S_ti_geo.append(ST)
+                S_ti_ar.append(ST)
 
             payoff_g.append(max(gmean(S_ti_geo)-K, 0))            
             payoff_a.append(max(np.mean(S_ti_ar)-K, 0))            
@@ -97,20 +95,19 @@ def asian_MC(S,N,T,sigma,r,K, n, type_op, param):
         K_ = np.repeat(K,n)
         sigma_= np.repeat(sigma,n)
         N_= np.repeat(N,n)
+        n_ = np.repeat(n, n)
 
-        control = np.exp(-r * T) * (np.mean(payoff_a)+asian_anal(S,N,T,sigma,r,K)-np.mean(payoff_g))
-        #var_contr = np.exp(-r * T)**2 * ( np.var(payoff_a) + np.var(payoff_g) - 2*np.cov(payoff_a, payoff_g))
-        #var_MC = np.var(payoff_a)
-        #print(len(np.exp(-r * T) * np.asarray(payoff_a)),
-        #len(np.exp(-r * T) * np.asarray(payoff_g)),
-        #len(np.exp(-r * T) * (np.asarray(payoff_a)+asian_anal(S,N,T,sigma,r,K)-np.asarray(payoff_g))),
-        #len(sim), len(K_), len(sigma_), len(N_))
+        control = np.exp(-r * T) * (np.mean(payoff_a)-np.mean(payoff_g))+asian_anal(S,N,T,sigma,r,K)
+        var_contr = np.exp(-r * T)**2 * ( np.var(payoff_a) + np.var(payoff_g) - 2*(np.cov(payoff_a, payoff_g)[0,1]))
+        var_MC = np.exp(-r * T)**2 * np.var(payoff_a)
+        #print(asian_anal(S,N,T,sigma,r,K)-np.asarray(payoff_g))
         data = {"Arithmetic":np.exp(-r * T) * np.asarray(payoff_a),
         "Geometric":np.exp(-r * T) * np.asarray(payoff_g),
         "Control": np.exp(-r * T) * (np.asarray(payoff_a)+asian_anal(S,N,T,sigma,r,K)-np.asarray(payoff_g)),
         "K":K_,
         "sigma": sigma_,
-        "N": N_}
+        "N": N_,
+        "n":n_,}
         df = pd.DataFrame(data)
         if param == "K":
             df.to_csv(f"K/asian_MC_{n}_{K}_{sigma}_{N}.csv")
@@ -118,11 +115,13 @@ def asian_MC(S,N,T,sigma,r,K, n, type_op, param):
             df.to_csv(f"N/asian_MC_{n}_{K}_{sigma}_{N}.csv")
         elif param == "sigma":
             df.to_csv(f"sigma/asian_MC_{n}_{K}_{sigma}_{N}.csv")
+        elif param == "n":
+            df.to_csv(f"path/asian_MC_{n}_{K}_{sigma}_{N}.csv")
         elif param == "none":
             df.to_csv(f"asian_MC_{n}_{K}_{sigma}_{N}.csv")
         
-
-        return control, np.exp(-r * T) * (np.mean(payoff_a))
+        #print(np.mean(asian_anal(S,N,T,sigma,r,K)-np.asarray(payoff_g)))
+        return control, np.exp(-r * T) * (np.mean(payoff_a)), var_contr, var_MC
 
     option_values = np.exp(-r * T) * np.asarray(payoff)
     #print(option_values)
@@ -169,7 +168,7 @@ print(result)
 result.to_csv("asian_MC_final")
 #%%
 """
-1.1: plot for comparing analytical and MC values
+3.1: plot for comparing analytical and MC values
 """
 
 #df = pd.read_csv("jToverN/asian_MC_final")
@@ -204,9 +203,9 @@ r = 0.06
 sigma = 0.2
 T = 1
 N = 365
-n = 10000
+n = 1000
 
-MC = asian_MC(S,N,T,sigma,r,K, n, type_op="control")
+MC = asian_MC(S,N,T,sigma,r,K, n, type_op="control", param="None")
 contr = MC[0]
 asian_mont = MC[1]
 
@@ -217,65 +216,142 @@ print(contr, asian_mont)
 """
 3.3.b: different parameter settings.
 """
-K = 99
 S = 100
 r = 0.06
 sigma = 0.2
 T = 1
 N = 365
-n = 100
+n = 10000
 
 # strike
 control = []
 asian_mont = []
+var_c = []
+var_MC = []
 k = np.linspace(50, 99, 99-50+1)
 for K in k:
     MC = asian_MC(S,N,T,sigma,r,K, n, type_op = "control", param="K")
     control.append(MC[0])
     asian_mont.append(MC[1])
+    var_c.append(MC[2])
+    var_MC.append(MC[3])
 
 frames = [ pd.read_csv(f"K/asian_MC_{n}_{K}_{sigma}_{N}.csv") for K in k ]
 result = pd.concat(frames)
 print(result)
 result.to_csv("asian_MC_final_K")
 
-sns.lineplot(data=result, x="K", y="Arithmetic", label = "Control variate")
-sns.lineplot(data=result, x="K", y="Control", label = "MC only")
+data = {"K": k, "var_c":var_c, "var_MC": var_MC}
+df = pd.DataFrame(data) 
+df.to_csv(f"K/asian_var_K.csv")
+
+sns.lineplot(data=result, x="K", y="Arithmetic", label = "MC")
+sns.lineplot(data=result, x="K", y="Control", label = "Control variate")
+plt.ylabel("Option value")
 plt.legend()
 plt.savefig("Asian_3_31.pdf")
 plt.show()
 
+plt.plot(k, var_MC, label="MC")
+plt.plot(k, var_c, label="Control")
+plt.xlabel("K", fontsize=14)
+plt.ylabel("Variance", fontsize=14)
+plt.legend()
+plt.savefig("Asian_3_31_var.pdf")
+plt.show()
+
 #%%
-# number of paths
+# number of timesteps
 K = 99
 S = 100
 r = 0.06
 sigma = 0.2
 T = 1
-N = 365
-n = 100
-NN = np.linspace(2, 365, 365-2+1)
+n = 10000
+#NN = np.linspace(2, 365*2, 365*2-2+1)
+NN = [365, 52, 24, 12, 6]
 control = []
 asian_mont = []
+var_c = []
+var_MC = []
 
 for N in NN:
     MC = asian_MC(S,N,T,sigma,r,K, n, type_op = "control", param="N")
     control.append(MC[0])
     asian_mont.append(MC[1])
+    var_c.append(MC[2])
+    var_MC.append(MC[3])
 
 frames = [ pd.read_csv(f"N/asian_MC_{n}_{K}_{sigma}_{N}.csv") for N in NN ]
 result = pd.concat(frames)
 print(result)
 result.to_csv("N/asian_MC_final_N")
 
-sns.lineplot(data=result, x="N", y="Arithmetic", label = "Control variate")
-sns.lineplot(data=result, x="N", y="Control", label = "MC only")
+data = {"N": NN, "var_c":var_c, "var_MC": var_MC}
+df = pd.DataFrame(data) 
+df.to_csv(f"N/asian_var_N.csv")
+    
+sns.lineplot(data=result, x="N", y="Arithmetic", label = "MC")
+sns.lineplot(data=result, x="N", y="Control", label = "Control variate")
+plt.ylabel("Option value")
 plt.legend()
 plt.savefig("Asian_3_32.pdf")
 plt.show()
 
+plt.plot(NN, var_MC, label="MC")
+plt.plot(NN, var_c, label="Control")
+plt.xlabel("N", fontsize=14)
+plt.ylabel("Variance", fontsize=14)
+plt.legend()
+plt.savefig("Asian_3_32_var.pdf")
+plt.show()
 #%%
 # sigma
+K = 99
+S = 100
+r = 0.06
+T = 1
+N = 365
+n = 10000
+vol = np.linspace(0.1, 1, 10)
+control = []
+asian_mont = []
+var_c = []
+var_MC = []
+
+for sigma in vol:
+    MC = asian_MC(S,N,T,sigma,r,K, n, type_op = "control", param="sigma")
+    control.append(MC[0])
+    asian_mont.append(MC[1])
+    var_c.append(MC[2])
+    var_MC.append(MC[3])
+
+frames = [ pd.read_csv(f"sigma/asian_MC_{n}_{K}_{sigma}_{N}.csv") for sigma in vol ]
+result = pd.concat(frames)
+print(result)
+result.to_csv("sigma/asian_MC_final_sigma")
+
+data = {"sigma": vol, "var_c":var_c, "var_MC": var_MC}
+df = pd.DataFrame(data) 
+df.to_csv(f"sigma/asian_var_sigma.csv")
+
+sns.lineplot(data=result, x="sigma", y="Arithmetic", label = "MC")
+sns.lineplot(data=result, x="sigma", y="Control", label = "Control variate")
+plt.ylabel("Option value")
+plt.legend()
+plt.savefig("Asian_3_33.pdf")
+plt.show()
+
+
+plt.plot(vol, var_MC, label="MC")
+plt.plot(vol, var_c, label="Control")
+plt.xlabel("sigma",  fontsize=14)
+plt.ylabel("Variance", fontsize=14)
+plt.legend()
+plt.savefig("Asian_3_33_var.pdf")
+plt.show()
+#%%
+# number of path
 K = 99
 S = 100
 r = 0.06
@@ -283,23 +359,65 @@ sigma = 0.2
 T = 1
 N = 365
 n = 100
-vol = np.linspace(0.0001, 5, 50)
+nn = [100,500, 1000,5000, 10000, 50000, 100000, 500000]
 control = []
 asian_mont = []
+var_c = []
+var_MC = []
 
-for sigma in vol:
-    MC = asian_MC(S,N,T,sigma,r,K, n, type_op = "control", param="sigma")
+for n in nn:
+    MC = asian_MC(S,N,T,sigma,r,K, n, type_op = "control", param="n")
     control.append(MC[0])
     asian_mont.append(MC[1])
+    var_c.append(MC[2])
+    var_MC.append(MC[3])
 
-frames = [ pd.read_csv(f"sigma/asian_MC_{n}_{K}_{sigma}_{N}.csv") for sigma in vol ]
+frames = [ pd.read_csv(f"path/asian_MC_{n}_{K}_{sigma}_{N}.csv") for n in nn ]
 result = pd.concat(frames)
 print(result)
-result.to_csv("sigma/asian_MC_final_sigma")
+result.to_csv("path/asian_MC_final_n")
 
-sns.lineplot(data=result, x="sigma", y="Arithmetic", label = "Control variate")
-sns.lineplot(data=result, x="sigma", y="Control", label = "MC only")
+data = {"path": nn, "var_c":var_c, "var_MC": var_MC}
+df = pd.DataFrame(data) 
+df.to_csv(f"path/asian_var_path.csv")
+
+sns.lineplot(data=result, x="n", y="Arithmetic", label = "MC")
+sns.lineplot(data=result, x="n", y="Control", label = "Control variate")
+plt.ylabel("Option value")
+plt.xscale("log")
 plt.legend()
-plt.savefig("Asian_3_33.pdf")
+plt.savefig("Asian_3_34.pdf")
+plt.show()
+
+plt.plot(nn, var_MC, label="MC")
+plt.plot(nn, var_c, label="Control")
+plt.xscale("log")
+plt.xlabel("n", fontsize=14)
+plt.ylabel("Variance", fontsize=14)
+plt.legend()
+plt.savefig("Asian_3_34_var.pdf")
+plt.show()
+#%%
+df = pd.read_csv("path/asian_MC_final_n")
+sd_con = []
+sd_MC = []
+for n in nn:
+    sd_con.append(np.std(df.loc[df["n"]==n]["Control"]))
+    sd_MC.append(np.std(df.loc[df["n"]==n]["Arithmetic"]))
+plt.plot(nn, var_MC, label="MC")
+plt.plot(nn, var_c, label="Control")
+plt.xscale("log")
+plt.xlabel("n", fontsize=14)
+plt.ylabel("Variance", fontsize=14)
+plt.legend()
+#plt.savefig("Asian_3_34_var.pdf")
+plt.show()
+plt.plot(nn, sd_MC, label="MC")
+plt.plot(nn, sd_con, label="Control")
+plt.xscale("log")
+plt.xlabel("n", fontsize=14)
+plt.ylabel("SD", fontsize=14)
+plt.legend()
+#plt.savefig("Asian_3_34_var.pdf")
 plt.show()
 #%%
