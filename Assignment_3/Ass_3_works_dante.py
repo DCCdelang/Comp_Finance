a4516a56eb8196b12727bab7e7d7769f506fcc8f
DCCdelang <33282294+DCCdelang@users.noreply.github.com>
2021-03-19 15:36:01 +0100

    change

--- Assignment_3/Ass_3_works_dante.py
+++ Assignment_3/Ass_3_works_dante.py
@@ -2,162 +2,111 @@ import math
 import random
 import numpy as np
 from scipy.stats import norm
-from tqdm import tqdm as _tqdm
 import copy
-
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib import colors
-
 import seaborn as sns
 sns.set(color_codes=True)
 
-def tqdm(*args, **kwargs):
-    return _tqdm(*args, **kwargs, mininterval=1)
-
-# Functions for Black-Scholes formulas
+# Black-Scholes formulas
 def option_value_bs(St, K, T, sigma, r, t=0):
     d1 = (math.log(St/K) + (r + sigma ** 2 * 0.5) * (T - t)) / (sigma * math.sqrt(T - t))
     d2 = d1 - sigma * math.sqrt(T - t)
-    
     return St * norm.cdf(d1) - K * math.exp(-r * (T - t)) * norm.cdf(d2)
 
 def hedge_parameter_bs(St, K, T, sigma, r, t=0):
     return norm.cdf((math.log(St/K) + (r + sigma ** 2 * 0.5) * (T - t)) / (sigma * math.sqrt(T - t)))
 
-def FD_Schemes(S0=100, K=110, T=1, v=0.3, r=0.04, M1=-5, M2=7, N_X=1000, N_T=1000, scheme='FTCS'):
-    assert scheme in ['FTCS', 'CN'], 'Not a valid scheme type'
+def Europe_Call_FD(S0=100, K=110, T=1, sigma=0.3, r=0.04, Exp1=-5, Exp2=7, M=1000, N=1000, scheme='FTCS'):
     # Initialization
-    X0 = math.log(S0)
-    
-    grid = np.zeros((N_X+2, N_T+1))
-    x_values = np.linspace(M1, M2, N_X+2)
-    
-    delta_x = x_values[2] - x_values[1]
-    delta_tau = T / float(N_T)
-    
-    x_index = np.arange(1, N_X+1)
-    tau_index = np.arange(1, N_T+1)
-    
+    X0 = math.log(S0)   
+    V = np.zeros((M+2, N+1))
+    x_list = np.linspace(Exp1, Exp2, M+2)
+    t_list = np.arange(1, N+1)
+
+    dx = x_list[2] - x_list[1]
+    dt = T / float(N)
+
     # Setup boundaries on first column and last row
-    grid[:, 0] = np.maximum(np.exp(x_values) - K, 0)
-    grid[-1, 1:] = (np.exp(M2) - K) * np.exp(-r * tau_index * delta_tau)
-    
-    # Define coefficients to use in both schemes
-    a = (2 * r - v**2) * delta_tau / (4 * delta_x)
-    b = v**2 * delta_tau / (2 * delta_x**2)
-    c = r * delta_tau + v**2 * delta_tau / (delta_x**2)
+    V[:, 0] = np.maximum(np.exp(x_list) - K, 0)
+    V[-1, 1:] = (np.exp(Exp2) - K) * np.exp(-r * t_list * dt)
     
     # Compute coefficients and construct matrices A and B
-    if scheme is 'FTCS':
-        alpha =  - a + b
-        beta = 1 - c
-        gamma = a + b
+    if scheme == 'FTCS':
+        alpha =  - (( r - 0.5* sigma**2) * dt / (2 * dx)) + (sigma**2   * dt / (2 * dx**2))
+        beta = 1 - (r * dt + sigma**2 * dt / (dx**2))
+        gamma = (( r - 0.5* sigma**2) * dt / (2 * dx)) + (sigma**2   * dt / (2 * dx**2))
         
-        A = np.diag([alpha for i in range(N_X-1)], -1) + \
-            np.diag([beta for i in range(N_X)]) + \
-            np.diag([gamma for i in range(N_X-1)], 1)
+        A = np.diag([alpha for i in range(M-1)], -1) + \
+            np.diag([beta for i in range(M)]) + \
+            np.diag([gamma for i in range(M-1)], 1)
         
-        B = np.identity(N_X)
+        B = np.identity(M)
         B_inv = B
-    else:
-        alpha = (a - b) / 2
-        beta = c / 2
-        gamma = (- a - b) / 2
+        for t in t_list:
+            V_prev = V[1:-1, t-1]
+            k_new = np.zeros(M)
+            k_prev = np.zeros(M)
+            k_prev[0] = 0
+            k_prev[-1] = gamma * V[-1, t-1]
+
+            # Compute V^{n+1}
+            V[1:-1, t] = B_inv.dot(A.dot(V_prev) + k_prev - k_new)
+
+    elif scheme == "CN":
+        alpha = ((( r - 0.5* sigma**2) * dt / (2 * dx)) - (sigma**2   * dt / (2 * dx**2))) / 2
+        beta = (r * dt + sigma**2 * dt / (dx**2)) / 2
+        gamma = (- (( r - 0.5* sigma**2) * dt / (2 * dx)) - (sigma**2   * dt / (2 * dx**2))) / 2
         
-        A = np.diag([-alpha for i in range(N_X-1)], -1) + \
-            np.diag([1-beta for i in range(N_X)]) + \
-            np.diag([-gamma for i in range(N_X-1)], 1)
+        A = np.diag([-alpha for i in range(M-1)], -1) + \
+            np.diag([1-beta for i in range(M)]) + \
+            np.diag([-gamma for i in range(M-1)], 1)
         
-        B = np.diag([alpha for i in range(N_X-1)], -1) + \
-            np.diag([1+beta for i in range(N_X)]) + \
-            np.diag([gamma for i in range(N_X-1)], 1)
+        B = np.diag([alpha for i in range(M-1)], -1) + \
+            np.diag([1+beta for i in range(M)]) + \
+            np.diag([gamma for i in range(M-1)], 1)
         B_inv = np.linalg.inv(B)
     
-    # Traverse the grid
-    for t in tau_index:
-        # Get V^n from grid
-        V_prev = grid[1:-1, t-1]
-        
-        if scheme is 'FTCS':
-            # k_1 is 0
-            k_new = np.zeros(N_X)
-            
-            # Construct k_2
-            k_prev = np.zeros(N_X)
-            k_prev[0] = 0
-            k_prev[-1] = gamma * grid[-1, t-1]
-        else:
-            # Construct k_1
-            k_new = np.zeros(N_X)
+        # Traverse the V
+        for t in t_list:
+            V_prev = V[1:-1, t-1]
+            k_new = np.zeros(M)
             k_new[0] = 0
-            k_new[-1] = gamma * grid[-1, t]
-            
-            # Construct k_2
-            k_prev = np.zeros(N_X)
+            k_new[-1] = gamma * V[-1, t]
+            k_prev = np.zeros(M)
             k_prev[0] = 0
-            k_prev[-1] = -gamma * grid[-1, t-1]
-        
-        # Compute V^{n+1}
-        grid[1:-1, t] = B_inv.dot(A.dot(V_prev) + k_prev - k_new)
+            k_prev[-1] = -gamma * V[-1, t-1]
+            
+            # Compute V^{n+1}
+            V[1:-1, t] = B_inv.dot(A.dot(V_prev) + k_prev - k_new)
     
-    # Interpolate value of option by looking at the last column of the grid
-    opt_value = np.interp(X0, x_values, grid[:, -1])
-    print(tau_index.shape, grid.shape, x_index.shape,)
-    return opt_value, grid, np.exp(x_values), delta_x, delta_tau
-
-f_M1 = -5
-f_M2 = 7
-f_N_X = 1000
-f_N_T = 1000
-print("\n",FD_Schemes(S0=110, K=110, T=1, v=0.3, r=0.04, M1=-5, M2=7, N_X=1000, N_T=1000, scheme='FTCS')[0],"\n")
+    # Get option value
+    option_value = np.interp(X0, x_list, V[:, -1])
+    
+    return option_value, V, np.exp(x_list), dx, dt
 
 
-bs_opt_value = option_value_bs(100, 110, 1, 0.3, 0.04)
-
-# opt_val, grid, _, dX, dT = FD_Schemes(S0=100, K=110, T=1, v=0.3, r=0.04,
-#                                       M1=f_M1, M2=f_M2, N_X=f_N_X, N_T=f_N_T, scheme='FTCS')
-# print('Option value: %.4f     Delta_X: %.4f     Delta_tau: %.4f' % (opt_val, dX, dT))
-# error = (opt_val / bs_opt_value - 1) * 100
-# print('Relative error: %.4f%%' % error)
-
-# opt_val, grid, _, dX, dT = FD_Schemes(S0=100, K=110, T=1, v=0.3, r=0.04,
-#                                       M1=f_M1, M2=f_M2, N_X=f_N_X, N_T=f_N_T, scheme='CN')
-# print('Option value: %.4f     Delta_X: %.4f     Delta_tau: %.4f' % (opt_val, dX, dT))
-# error = (opt_val / bs_opt_value - 1) * 100
-# print('Relative error: %.4f%%' % error)
-
-# bs_opt_value_2 = option_value_bs(110, 110, 1, 0.3, 0.04)
-
-
-# opt_val, grid, _, dX, dT = FD_Schemes(S0=110, K=110, T=1, v=0.3, r=0.04,
-#                                       M1=f_M1, M2=f_M2, N_X=f_N_X, N_T=f_N_T, scheme='FTCS')
-# print('Option value: %.4f     Delta_X: %.4f     Delta_tau: %.4f' % (opt_val, dX, dT))
-# error = (opt_val / bs_opt_value_2 - 1) * 100
-# print('Relative error: %.4f%%' % error)
-
-def plot_3d_grid(S0, K, T, v, r, M1, M2, N_X, N_T, scheme, restrict=True, savefig=False):
-    print(f'Scheme {scheme}')
-    opt_value, grid, _, _, _ = FD_Schemes(S0=S0, K=K, T=T, v=v, r=r,
-                                          M1=M1, M2=M2, N_X=N_X, N_T=N_T, scheme=scheme)
+def plot_3D(S0, K, T, sigma, r, Exp1, Exp2, M, N, scheme, restrict=True, savefig=False):
+    _, V, _, _, _ = Europe_Call_FD(S0=S0, K=K, T=T, sigma=sigma, r=r, Exp1=Exp1, Exp2=Exp2, M=M, N=N, scheme=scheme)
     
     fig = plt.figure(figsize=(10, 8))
     ax = Axes3D(fig)
     if restrict:
-        x_range = np.linspace(M1, M2, f_N_X+2)
+        x_range = np.linspace(Exp1, Exp2, M+2)
         idx = np.where(x_range > 3.5)[0][0]
         idx2 = np.where(x_range > 5)[0][0]
-        print(idx,idx2)
-        grid = grid[idx:idx2, :]
+        V = V[idx:idx2, :]
     else:
         idx = 0
+        idx2 = -1
     S_list = []
-    for value in np.linspace(M1,M2, N_X+2)[idx:idx2]:
+    for value in np.linspace(Exp1,Exp2, M+2)[idx:idx2]:
         S_list.append(np.exp(value))
-    t, S = np.meshgrid(np.linspace(0, 1, N_T+1), S_list)
+    t, S = np.meshgrid(np.linspace(0, 1, N+1), S_list)
     
-    ax.plot_surface(S, t, grid, cmap='coolwarm', linewidth=0, antialiased=True)
+    ax.plot_surface(S, t, V, cmap='binary', linewidth=0, antialiased=True)
     ax.view_init(20, 100)
     ax.set_xlabel('S')
     ax.set_ylabel('tau')
@@ -166,11 +115,39 @@ def plot_3d_grid(S0, K, T, v, r, M1, M2, N_X, N_T, scheme, restrict=True, savefi
         plt.savefig(f'3d_grid_{scheme}_restrict_{restrict}.png', dpi=200)
     plt.show()
 
+Exp_low = -6
+Exp_high = 8
+M = 1000
+N = 1000
+
+# # r = 4%; vol = 30%; S0 = 100; K = 110;
+# bs_opt_value = option_value_bs(100, 110, 1, 0.3, 0.04)
+# print("Black scholes value:",bs_opt_value)
+
+# opt_val, V, _, dX, dT = Europe_Call_FD(S0=100, K=110, T=1, sigma=0.3, r=0.04, Exp1=Exp_low, Exp2=Exp_high, M=M, N=N, scheme='FTCS')
+# print('FTCS option value: %.4f   \nDelta_X: %.4f   \nDelta_tau: %.4f' % (opt_val, dX, dT))
+# error = (opt_val / bs_opt_value - 1) * 100
+# print('Relative error: %.4f%%' % error)
+
+# # r = 4%; vol = 30%; S0 = 110; K = 110
+# bs_opt_value = option_value_bs(110, 110, 1, 0.3, 0.04)
+# print("Black scholes value:",bs_opt_value)
+
+# opt_val, V, _, dX, dT = Europe_Call_FD(S0=110, K=110, T=1, sigma=0.3, r=0.04, Exp1=Exp_low, Exp2=Exp_high, M=M, N=N, scheme='FTCS')
+# print('FTCS option value: %.4f   \nDelta_X: %.4f   \nDelta_tau: %.4f' % (opt_val, dX, dT))
+# error = (opt_val / bs_opt_value - 1) * 100
+# print('Relative error: %.4f%%' % error)
+
+# # r = 4%; vol = 30%; S0 = 120; K = 110
+# bs_opt_value = option_value_bs(120, 110, 1, 0.3, 0.04)
+# print("Black scholes value:",bs_opt_value)
+
+# opt_val, V, _, dX, dT = Europe_Call_FD(S0=120, K=110, T=1, sigma=0.3, r=0.04, Exp1=Exp_low, Exp2=Exp_high, M=M, N=N, scheme='FTCS')
+# print('FTCS option value: %.4f   \nDelta_X: %.4f   \nDelta_tau: %.4f' % (opt_val, dX, dT))
+# error = (opt_val / bs_opt_value - 1) * 100
+# print('Relative error: %.4f%%' % error)
 
 
-f_M1 = -5
-f_M2 = 7
-f_N_X = 1000
-f_N_T = 1000
+plot_3D(S0=100, K=110, T=1, sigma=0.3, r=0.04, Exp1=Exp_low, Exp2=Exp_high, M=M, N=N, scheme='FTCS', restrict=True)
 
-plot_3d_grid(S0=100, K=110, T=1, v=0.3, r=0.04, M1=f_M1, M2=f_M2, N_X=f_N_X, N_T=f_N_T, scheme='CN', restrict=True)
+plot_3D(S0=110, K=110, T=1, sigma=0.3, r=0.04, Exp1=Exp_low, Exp2=Exp_high, M=M, N=N, scheme='CN', restrict=True)
