eca987e4305b064b17d19651d058edff8dbf5a0a
DCCdelang <33282294+DCCdelang@users.noreply.github.com>
2021-03-21 10:18:52 +0100

    change

--- Assignment_3/plot.py
+++ Assignment_3/plot.py
@@ -13,29 +13,6 @@ Exp_high = 8
 M = 1000
 N = 1000
 
-def plot_2D(S0, K, T, sigma, r, Exp1, Exp2, M, N, scheme, restrict=True, savefig=False):
-    _, V, _, _, _ = Europe_Call_FD(S0=S0, K=K, T=T, sigma=sigma, r=r, Exp1=Exp1, Exp2=Exp2, M=M, N=N, scheme=scheme)
-
-    if restrict:
-        x_range = np.linspace(Exp1, Exp2, M+2)
-        idx = np.where(x_range > 3.5)[0][0]
-        idx2 = np.where(x_range > 5)[0][0]
-        V = V[idx:idx2, :]
-    else:
-        idx = 0
-        idx2 = -1
-
-    S_list = []
-    for value in np.linspace(Exp1,Exp2, M+2)[idx:idx2]:
-        S_list.append(np.exp(value))
-
-    plt.plot(S_list, V[0])
-    plt.show()
-    
-    t, S = np.meshgrid(np.linspace(0, 1, N+1), S_list)
-
-
-
 def plot_3D(S0, K, T, sigma, r, Exp1, Exp2, M, N, scheme, restrict=True, savefig=False):
     _, V, _, _, _ = Europe_Call_FD(S0=S0, K=K, T=T, sigma=sigma, r=r, Exp1=Exp1, Exp2=Exp2, M=M, N=N, scheme=scheme)
     
@@ -110,9 +87,7 @@ def compute_and_plot_delta(S0=100, K=110, T=1, sigma=0.3, r=0.04, Exp1=-5, Exp2=
 # compute_and_plot_delta(M=2000, N=1000, scheme='CN', restrict=True, savefig=False)
 
 """ 3D plot """
-# plot_3D(S0=100, K=110, T=1, sigma=0.3, r=0.04, Exp1=Exp_low, Exp2=Exp_high, M=M, N=N, scheme='FTCS', restrict=True)
+plot_3D(S0=100, K=110, T=1, sigma=0.3, r=0.04, Exp1=Exp_low, Exp2=Exp_high, M=M, N=N, scheme='FTCS', restrict=True)
 
 # plot_3D(S0=110, K=110, T=1, sigma=0.3, r=0.04, Exp1=Exp_low, Exp2=Exp_high, M=M, N=N, scheme='CN', restrict=True)
 
-""" 2D plot"""
-plot_2D(S0=100, K=110, T=1, sigma=0.3, r=0.04, Exp1=Exp_low, Exp2=Exp_high, M=M, N=N, scheme='FTCS', restrict=True)
